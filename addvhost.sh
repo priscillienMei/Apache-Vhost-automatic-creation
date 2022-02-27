78447538ff5032e60ad75770af692f2aaba84144
Priscillien <priscillien@meideck.net>
2022-02-27 14:55:41 +0100

    add SSL cert generation

--- addvhost.sh
+++ addvhost.sh
@@ -1,27 +1,37 @@
 #!/bin/bash
+#Todo
+# [ ] add choice is www or not as ServerAlias
+# [X] Ask for letsencrypt SSL certificate creation
+# [ ] If not asked for www as server alias do not generate www cert
+#
+
+EMAIL="email@domain.com"
+
 if [ "$EUID" -ne 0 ]
-    then 
+    then
         echo "Please run as root"
         exit
-    else 
+    else
         if [ "$1" = '' ]
            then
                echo "Please enter the name for the virtual host"
            else
-           
+
+ echo "Create folder for vhost files"
  mkdir /var/www/$1
- 
+
+ echo "Create vhost configuration file"
  sudo echo '<VirtualHost *:80>
  ServerName '$1'
  ServerAlias 'www.$1'
  DocumentRoot '/var/www/$1'
- 
+
  <Directory '/var/www/$1'>
         Options -Indexes +FollowSymLinks +MultiViews
         AllowOverride All
         Require all granted
  </Directory>
- 
+
 <FilesMatch \.php$>
         # 2.4.10+ can proxy to unix socket
         SetHandler "proxy:unix:/var/run/php/php7.4-fpm.sock|fcgi://localhost"
@@ -31,9 +41,28 @@ if [ "$EUID" -ne 0 ]
     CustomLog ${APACHE_LOG_DIR}/'$1'_access.log combined
 
  </VirtualHost>' > /etc/apache2/sites-available/$1.conf
+
+ #Install SSL cert ?
+ while true; do
+     read -p "Do you want to generate and install SSL cert? " php
+     case $php in
+         [Yy]* )
+              REQUIRED_PKG="python3-certbot-apache"
+              PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
+              echo Checking for $REQUIRED_PKG: $PKG_OK
+              if [ "" = "$PKG_OK" ]; then
+                echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
+                sudo apt-get --yes install $REQUIRED_PKG
+              fi
+              certbot --apache --non-interactive --agree-tos -m $EMAIL -d $1 -d www.$1 --redirect --expand;
+         break;;
+         [Nn]* ) break;;
+         * ) echo "Please answer yes/Y/y or no/N/n.";;
+     esac
+ done
+ 
     sudo a2ensite $1
     sudo systemctl reload apache2
     echo "Your virtual host $1 has been set up"
        fi
 fi
-
